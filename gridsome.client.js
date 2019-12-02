c8b42897b73e49f721b10cef4c42ec077850f37f
Brandon Pittman <brandon@brandonpittman.net>
2019-12-02 19:07:58 +0900

    Initial publish

---
+++ gridsome.client.js
@@ -0,0 +1,26 @@
+export default function (_, options, {isClient}) {
+  if (!options.siteId) {
+    console.error('siteId not set')
+    return
+  }
+
+  if (isClient) {
+    (function (f, a, t, h, o, m) {
+      a[h] =
+        a[h] ||
+        function () {
+          (a[h].q = a[h].q || []).push(arguments);
+        };
+
+      (o = f.createElement('script')),
+        (m = f.getElementsByTagName('script')[0]);
+      o.async = 1;
+      o.src = t;
+      o.id = 'fathom-script';
+      m.parentNode.insertBefore(o, m);
+    })(document, window, '//cdn.usefathom.com/tracker.js', 'fathom');
+
+    window.fathom('set', 'siteId', options.siteId);
+    window.fathom('trackPageview');
+  }
+}
