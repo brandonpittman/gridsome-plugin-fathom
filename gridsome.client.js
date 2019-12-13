463f2798fff88eafc1c958f7643881ba31c73f29
Brandon Pittman <brandon@brandonpittman.net>
2019-12-13 17:19:59 +0900

    Add  function

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,3 +1,27 @@
+function trackGoal(id) {
+  window.fathom('trackGoal', id, 0);
+}
+
+void function setupFathom() {
+  (function (f, a, t, h, o, m) {
+    a[h] =
+      a[h] ||
+      function () {
+        (a[h].q = a[h].q || []).push(arguments);
+      };
+
+    (o = f.createElement('script')),
+      (m = f.getElementsByTagName('script')[0]);
+    o.async = 1;
+    o.src = t;
+    o.id = 'fathom-script';
+    m.parentNode.insertBefore(o, m);
+  })(document, window, '//cdn.usefathom.com/tracker.js', 'fathom');
+
+  window.fathom('set', 'siteId', options.siteId);
+  window.fathom('trackPageview');
+}
+
 export default function (Vue, options, {isClient}) {
   if (!options.siteId) {
     console.error('siteId not set')
@@ -6,37 +30,14 @@ export default function (Vue, options, {isClient}) {
 
   if (!isClient) return
 
-  // dummy function
-  Vue.prototype.$trackGoal = _ => {}
+  Vue.prototype.$trackGoal = _ => {
 
-  if (options.host && !options.host != window.location.host) return
+    if (options.host && (options.host != window.location.host)) return
 
-  if (options.debug || !window.location.host.startsWith('localhost')) {
-    // set up window.fathom
-    (function (f, a, t, h, o, m) {
-      a[h] =
-        a[h] ||
-        function () {
-          (a[h].q = a[h].q || []).push(arguments);
-        };
+    if (options.debug || !window.location.host.startsWith('localhost')) {
+      setupFathom();
 
-      (o = f.createElement('script')),
-        (m = f.getElementsByTagName('script')[0]);
-      o.async = 1;
-      o.src = t;
-      o.id = 'fathom-script';
-      m.parentNode.insertBefore(o, m);
-    })(document, window, '//cdn.usefathom.com/tracker.js', 'fathom');
-
-    window.fathom('set', 'siteId', options.siteId);
-    window.fathom('trackPageview');
-
-    // create goal helper function
-    Vue.prototype.$trackGoal = (id) => {
-      if (isClient) {
-        window.fathom('trackGoal', id, 0);
-      }
+      Vue.prototype.$trackGoal = trackGoal
     }
   }
 }
-
